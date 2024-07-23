432fb9fb840fe772a280083f01fe758094ec1bbc
dongapple <hg971027@gmail.com>
2024-07-23 17:49:21 +0900

    ✨ 메인 페이지 마크업

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,72 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+// 스타일드 컴포넌트 상단에 정의
+const Container = styled.div`
+  padding: 20px;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 2em;
+  color: #333;
+  color: #;
+`;
+
+const Nav = styled.nav`
+  margin-top: 20px;
+`;
+
+const List = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`;
+
+const ListItem = styled.li`
+  display: inline;
+  margin: 0 10px;
+  position: relative;
+
+  &::after {
+    content: '|';
+    position: absolute;
+    right: -12px; /* 막대와 텍스트 간격 조정 */
+    color: #333; /* 막대 색상 */
+  }
+
+  &:last-child::after {
+    content: ''; /* 마지막 항목에는 막대 표시 안 함 */
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #007bff;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/forgot_password">Forgot Password</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Container>
+      <Title>메인페이지 입니다.</Title>
+      <Nav>
+        <List>
+          <ListItem>
+            <StyledLink to="/login">로그인</StyledLink>
+          </ListItem>
+          <ListItem>
+            <StyledLink to="/forgot_password">비밀번호 찾기</StyledLink>
+          </ListItem>
+          <ListItem>
+            <StyledLink to="/signup">회원가입</StyledLink>
+          </ListItem>
+        </List>
+      </Nav>
+    </Container>
   );
 };
 
