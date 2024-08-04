ad8e3287e6ead5defdfe4f06f1f7fd0032248edc
dongapple <hg971027@gmail.com>
2024-08-05 02:25:43 +0900

    ✨ 환경변수 설정

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,28 +1,27 @@
-// src/firebaseConfig.ts
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { getAuth } from 'firebase/auth';
+import { fetchSignInMethodsForEmail, getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Firebase 구성 객체
 const firebaseConfig = {
-  apiKey: 'AIzaSyBdTPXgRfRxfgHspv4teLhY2Ueor1D38c0',
-  authDomain: 'loginimplement-8e324.firebaseapp.com',
-  projectId: 'loginimplement-8e324',
-  storageBucket: 'loginimplement-8e324.appspot.com',
-  messagingSenderId: '710538217051',
-  appId: '1:710538217051:web:2cdbfe3a70d2776bf1a17e',
-  measurementId: 'G-GJ6L6KHKR6',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
 
-// Analytics 초기화
+// Firebase Analytics 초기화
 const analytics = getAnalytics(app);
 
-// 인증, Firestore, Storage 서비스 초기화
+// Firebase 인증, Firestore, Storage 서비스 초기화
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
