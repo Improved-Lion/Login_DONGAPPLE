1a0195fb9e95b74d375aebadf21fbb3f2f89eee7
dongapple <hg971027@gmail.com>
2024-08-04 20:44:39 +0900

    ✨ 라우터에 lazy 추가

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Signup from '../pages/Signup';
-import ForgotPassword from '../pages/ForgotPassword';
+
+const Home = lazy(() => import('../pages/Home'));
+const Login = lazy(() => import('../pages/Login'));
+const Signup = lazy(() => import('../pages/Signup'));
+const ForgotPassword = lazy(() => import('../pages/ForgotPassword'));
 
 const AppRoutes: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/forgot_password" element={<ForgotPassword />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot_password" element={<ForgotPassword />} />
+      </Routes>
+    </Suspense>
   );
 };
 
